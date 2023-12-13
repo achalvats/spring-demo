fa331b419e9729e4186b647f10590639fbb49f31
Achal Vats <achalvats15@gmail.com>
2023-12-12 23:38:24 -0500

    adding gameconsole inferfance in main package

--- learn-spring-framework/learn-spring-framework/src/main/java/com/example/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/learn-spring-framework/src/main/java/com/example/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.spring.learnspringframework.game.ContraGame;
 import com.example.spring.learnspringframework.game.GameRunner;
+import com.example.spring.learnspringframework.game.GaminConsole;
 import com.example.spring.learnspringframework.game.MarioGame;
 
 @SpringBootApplication
@@ -12,8 +13,8 @@ public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
-		MarioGame game1 = new MarioGame();
-		ContraGame game2 = new ContraGame();
+		GaminConsole game1 = new MarioGame();
+		GaminConsole game2 = new ContraGame();
 		GameRunner runner1 = new GameRunner(game1);
 		GameRunner runner2 = new GameRunner(game2);
 		runner1.run();
